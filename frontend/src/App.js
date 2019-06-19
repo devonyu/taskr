724306d31c34cd661c9459e659f42dec0c15903e
Devon Yu <devonyu415@gmail.com>
2019-06-18 22:37:59 -0700

    testing eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
+
+import React from "react";
+import "./App.css";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Taskr App page
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/devonyu"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Create and modify your tasks!
-        </a>
-      </header>
+      <Homepage />
     </div>
   );
 }
---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,13 @@
+// @flow
+
+import React from "react";
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+      <h1>{"Splash page for TaskR"}</h1>
+    </div>
+  );
+}
+
+export default Homepage;
