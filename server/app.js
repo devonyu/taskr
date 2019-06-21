96b4f1be2a001786671642739ec33f0a521adbdb
Devon Yu <devonyu415@gmail.com>
2019-06-20 20:45:36 -0700

    should return right status code

--- server/app.js
+++ server/app.js
@@ -10,13 +10,13 @@ app.get('/ping', (req, res) => {
   res.status(418).send('pong');
 })
 
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!')
+})
+
 app.get('*', (req, res) => {
   console.log('catch all with *');
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
-
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!')
-})
 
 module.exports = app;
