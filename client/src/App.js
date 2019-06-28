093a5594f2569815f04b375e88323a9a1881de4f
Devon Yu <devonyu415@gmail.com>
2019-06-28 11:48:34 -0700

    prettier app before route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 // @flow
 
-import "./App.css";
+import './App.css';
 import Button from '@material-ui/core/Button';
-import Homepage from "./components/Homepage";
-import Navbar from "./components/Navbar";
-import React from "react";
-import SingleTask from "./components/SingleTask";
+import React from 'react';
+import Homepage from './components/Homepage';
+import Navbar from './components/Navbar';
+import SingleTask from './components/SingleTask';
 
 function App() {
   const [displayTask, toggleTask] = React.useState(false);
@@ -13,7 +13,13 @@ function App() {
     <div className="App">
       <Navbar />
       <Homepage />
-      <Button variant="contained" color="primary" onClick={() => toggleTask(!displayTask)}>Toggle Example Task</Button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => toggleTask(!displayTask)}
+      >
+        Toggle Example Task
+      </Button>
       {displayTask && <SingleTask />}
     </div>
   );
