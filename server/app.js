1076c14ce512cd5918c05c95235676327ae92dab
EC2 Default User <ec2-user@ip-172-31-10-82.us-west-1.compute.internal>
2019-11-07 01:29:07 +0000

    launch script for production on ec2

--- server/app.js
+++ server/app.js
@@ -170,10 +170,12 @@ app.put("/updatetask", (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
+console.log('is this production?');
+console.log(process.env.NODE_ENV);
     app.use(express.static(path.join(__dirname, '/../client/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
   });
 }
 
