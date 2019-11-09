dd173e9818991b0b9fbbae072b6039956b609e42
Devon Yu <devonyu415@gmail.com>
2019-11-09 12:22:40 -0800

    delete task working

--- server/app.js
+++ server/app.js
@@ -216,12 +216,13 @@ app.delete("/deletetask", (req, res) => {
       taskID
     }
   };
-  docClient.delete(params, function(err, data) {
+  docClient.delete(params, (err, data) => {
     if (err) {
       console.error(
         "Unable to delete item. Error JSON:",
         JSON.stringify(err, null, 2)
       );
+      res.status(404).send(JSON.stringify(err, null, 2));
     } else {
       console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
       res.status(200).send(JSON.stringify(data, null, 2));
